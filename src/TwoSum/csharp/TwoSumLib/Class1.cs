0ea5691685729a20d0ab5737d812989d55782b27
Danielkon96 <danielkon96@Yahoo.com>
2025-08-06 17:41:59 -0500

    solutions removed

--- src/TwoSum/csharp/TwoSumLib/Class1.cs
+++ src/TwoSum/csharp/TwoSumLib/Class1.cs
@@ -8,22 +8,7 @@ namespace TwoSumLib
     {
         public int[] TwoSum(int[] nums, int target) 
         {
-            Dictionary<int, int> numMap = new Dictionary<int, int>();
-
-            for (int i = 0; i < nums.Length; i++) 
-            {
-                int complement = target - nums[i];
-                if (numMap.ContainsKey(complement)) 
-                {
-                    return new int[] { numMap[complement], i };
-                }
-                if (!numMap.ContainsKey(nums[i])) 
-                {
-                    numMap.Add(nums[i], i);
-                }
-            }
-
-            throw new ArgumentException("No two sum solution");
+            throw new NotImplementedException("The TwoSum method is not yet implemented.");
         }
     }
 }
